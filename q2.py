327dd5195186fe1f97e758166d55fa4dd70f626e
YashJn29 <22103071@mail.jiit.ac.in>
2024-08-27 16:20:59 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,5 @@
+def count_letter(stri)
+return stri.count()
+
+string = "TCS is the largest IT service company in India, headquartered in Mumbai, and also the largest employee base"
+count_letter(string)
